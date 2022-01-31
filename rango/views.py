5b889d327857a903c9cb79c1c88d792ba2a9000c
“zoe1996g” <562198719@qq.com>
2022-01-31 14:01:44 +0000

    chaper 4 passed test successfully ^_^

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    # return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    # return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
