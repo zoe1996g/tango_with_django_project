d1410083662ab98c5217b2f0e8cc3b887dd54d80
“zoe1996g” <562198719@qq.com>
2022-02-13 00:20:57 +0000

    chaper 10 passed test successfully ^_^

--- rango/views.py
+++ rango/views.py
@@ -12,29 +12,41 @@ from rango.forms import PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 def index(request):
     # return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-
-    page_list = Page.objects.order_by('-views')[:5]
     context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    # request.session.set_test_cookie()
+    visitor_cookie_handler(request)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
     # return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
     # prints out whether the method is a GET or a POST
-    print(request.method)
+    # print(request.method)
     # prints out the user name, if no one is logged in it prints `AnonymousUser`
-    print(request.user)
-    return render(request, 'rango/about.html', {})
+    # print(request.user)
+
+    # if request.session.test_cookie_worked():
+        # print("TEST COOKIE WORKET!")
+        # request.session.delete_test_cookie()
+    # return render(request, 'rango/about.html', {})
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict)
+
+    return response
 
 
 def show_category(request, category_name_slug):
@@ -169,4 +181,29 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
-
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
